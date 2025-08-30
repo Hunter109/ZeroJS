6630fc7507f571cfbe81b380dd1ddb27cbfe137f
utsab wagle <utsavwagle508@gmail.com>
2025-08-30 09:28:47 +0545

    basic revision

---
+++ forloop.js
@@ -0,0 +1,21 @@
+// wrting code in. which sum of two digit is 10
+
+// for (let i = 1, j = 9; i <= 9, j >= 1; i++, j--) {
+//     console.log(`${i}${j}`)
+// }
+
+
+let array = [10, 20, 30, 40, 50];
+let length = array.length;
+console.log(length)
+
+
+// for of give direct value  
+for (let value of array) {
+    console.log(value)
+}
+
+// for in give index of array 
+for (let index in array) {
+    console.log(array[index])
+}
---
+++ ifEsle.js
@@ -0,0 +1,11 @@
+let age = 10;
+if (age) {
+    console.log(age)
+}
+
+// 0
+// false 
+// ''
+// undefined 
+// null
+// NAN

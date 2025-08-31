7cfa839173a02855b77014d0f008275553a3c55f
utsab wagle <utsavwagle508@gmail.com>
2025-08-31 07:08:01 +0545

    conditional problem practice

---
+++ conditional/problem1.js
@@ -0,0 +1,10 @@
+// Write a program that checks if a number is positive or negative.
+
+let num = prompt("enter a number");
+if (num >= 0) {
+    console.log("the number is postive")
+
+}
+else {
+    console.log("the number is negative")
+}
---
+++ conditional/problem2.js
@@ -0,0 +1,10 @@
+// Check if a given number is even or odd.
+
+let number = 29;
+
+if (number % 2 == 0) {
+    console.log("the givern number is even")
+}
+else {
+    console.log("the given number is odd")
+}
